1f1a81f47f63a57fc8364905413635799610cc3c
coelho.matheus@outlook.com <MatheusBispoDev>
2021-07-20 13:28:22 -0300

    Conhecendo ambiente/Criando formulário de transferência

---
+++ cursos-angular/conhecendo-framework-angular/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solcitada Nova Transferencia');
+    console.log('Valor:', this.valor);
+    console.log('Destino:', this.destino);
+  }
+}
