77387b6e5465618f7ed928927ca41a8f05eba10d
MatheusBispoDev <coelho.matheus@outlook.com>
2021-07-21 00:12:05 -0300

    #curso-SOLID att curso

---
+++ cursos-arquitetura-e-design-projetos-java/curso-SOLID/2051-solid-java-projeto_inicial/2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/service/ValidacaoReajuste.java
@@ -0,0 +1,5 @@
+package br.com.alura.rh.model.service;
+
+public interface ValidacaoReajuste {
+    
+}
---
+++ cursos-arquitetura-e-design-projetos-java/curso-SOLID/2051-solid-java-projeto_inicial/2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/service/validacaoPeriodicidadeReajuste.java
@@ -0,0 +1,21 @@
+package br.com.alura.rh.model.service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+public class validacaoPeriodicidadeReajuste {
+    public void validar(Funcionario funcionario, BigDecimal aumento) {
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+
+        long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+
+        if (mesesDesdeUltimoReajuste < 6) {
+            throw new ValidacaoException("Intervalo entre reajustes deve ser no minimo de 6 meses!");
+        }
+    }
+}
