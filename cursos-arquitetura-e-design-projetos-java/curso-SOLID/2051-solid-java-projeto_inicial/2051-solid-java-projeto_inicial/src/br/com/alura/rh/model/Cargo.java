a177f0691f508def3931952a0a1434c8b822690f
coelho.matheus@outlook.com <MatheusBispoDev>
2021-07-21 12:46:38 -0300

    #curso-SOLID: Implementando regra de negócio para promocao de funcionário

--- cursos-arquitetura-e-design-projetos-java/curso-SOLID/2051-solid-java-projeto_inicial/2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/Cargo.java
+++ cursos-arquitetura-e-design-projetos-java/curso-SOLID/2051-solid-java-projeto_inicial/2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/Cargo.java
@@ -1,10 +1,32 @@
 package br.com.alura.rh.model;
 
 public enum Cargo {
+	//Enum pode ter métodos para implementar regras de negócios
+	ASSISTENTE {
+		@Override
+		public Cargo getProximoCargp() {
+			return ANALISTA;
+		}
+	},
+	ANALISTA {
+		@Override
+		public Cargo getProximoCargp() {
+			return ESPECIALISTA;
+		}
+	},
+	ESPECIALISTA {
+		@Override
+		public Cargo getProximoCargp() {
+			return GERENTE;
+		}
+	},
+	GERENTE {
+		@Override
+		public Cargo getProximoCargp() {
+			return GERENTE;
+		}
+	};
 
-	ASSISTENTE,
-	ANALISTA,
-	ESPECIALISTA,
-	GERENTE;
+    public abstract Cargo getProximoCargp();
 
 }
