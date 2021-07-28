7663437f7f75b14a3dac00f869a058501fde6f3d
MatheusBispoDev <coelho.matheus@outlook.com>
2021-07-27 23:10:45 -0300

    #fundamentos-angular: Data binding e instalação de denpendências

--- cursos-angular/fundamentos-angular/alurapic/src/app/app.component.ts
+++ cursos-angular/fundamentos-angular/alurapic/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'AluraPic';
+  description = 'Esquilo';
+  url = 'https://pm1.narvii.com/6701/29aba39736d91e0173afb3a1c348900e585a2f43_hq.jpg'
 }
